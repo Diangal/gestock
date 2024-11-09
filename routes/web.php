3ddbfa1143c57de775c01f0e421a05b0dcf75e04
farma.kane <farma.kane@quadradiffusion2.onmicrosoft.com>
2024-11-09 22:04:56 +0000

    third

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GesController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('articles', [GesController::class, 'index'])->name('articles.index');
+
+Route::get('articles/create', [GesController::class, 'create'])->name('articles.create');
+
+Route::post('articles', [GesController::class, 'store'])->name('articles.store');
+
+Route::get('articles/{article}/edit', [GesController::class, 'edit'])->name('articles.edit');
+
+Route::put('articles/{article}', [GesController::class, 'update'])->name('articles.update');
+
+Route::delete('articles/{article}', [GesController::class, 'destroy'])->name('articles.destroy');
